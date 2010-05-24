dd961d751db6948677a729e8dcf8fcd587b7f97c
Sandro Turriate <sandro.turriate@gmail.com>
2010-05-23 21:18:16 -0400

    Cleanup

--- lib/ephemeral_response/net_http.rb
+++ lib/ephemeral_response/net_http.rb
@@ -11,7 +11,7 @@ module Net
       URI.parse("#{scheme}://#{conn_address}:#{conn_port}#{request.path}")
     end
 
-    def request(request, body = nil, &block)# (*args)
+    def request(request, body = nil, &block)
       EphemeralResponse::Fixture.respond_to(generate_uri(request), request.method) do
         connect_without_ephemeral_response
         request_without_ephemeral_response(request, body, &block)
