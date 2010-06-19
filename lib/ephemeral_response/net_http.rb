76157f3bb27db46ebe7aec9e80ac88639058dcd7
Sandro Turriate <sandro.turriate@gmail.com>
2010-06-19 00:46:59 -0400

    Use Net::HTTP debugger when connection established

--- lib/ephemeral_response/net_http.rb
+++ lib/ephemeral_response/net_http.rb
@@ -3,17 +3,21 @@ module Net
     alias request_without_ephemeral_response request
     alias connect_without_ephemeral_response connect
 
+    attr_accessor :uri
+
     def connect
     end
     private :connect
 
     def generate_uri(request)
       scheme = use_ssl? ? "https" : "http"
-      URI.parse("#{scheme}://#{conn_address}:#{conn_port}#{request.path}")
+      self.uri = URI.parse("#{scheme}://#{conn_address}:#{conn_port}#{request.path}")
     end
 
     def request(request, body = nil, &block)
-      EphemeralResponse::Fixture.respond_to(generate_uri(request), request.method) do
+      generate_uri(request)
+      EphemeralResponse::Fixture.respond_to(uri, request) do
+        D "EphemeralResponse: establishing connection to #{uri}"
         connect_without_ephemeral_response
         request_without_ephemeral_response(request, body, &block)
       end
