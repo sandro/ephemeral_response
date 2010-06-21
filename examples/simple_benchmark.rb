5a08a00730114e65eb866415dd286f99f86c68a3
Sandro Turriate <sandro.turriate@gmail.com>
2010-06-21 11:24:34 -0400

    Notes on simple_benchmark example

--- examples/simple_benchmark.rb
+++ examples/simple_benchmark.rb
@@ -6,6 +6,8 @@ require 'benchmark'
 EphemeralResponse::Configuration.expiration = 1
 EphemeralResponse.activate
 
+# Run benchmarks against thefuckingweather.com
+# The first request takes much longer than the rest
 def benchmark_request(number=1)
   uri = URI.parse('http://thefuckingweather.com/?RANDLOC=')
   time = Benchmark.realtime do
