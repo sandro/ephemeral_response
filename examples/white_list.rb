8a17e04f5ad36718b5d62d84957d5e605d8c0345
Sandro Turriate <sandro.turriate@gmail.com>
2010-06-21 13:20:22 -0400

    White list example

---
+++ examples/white_list.rb
@@ -0,0 +1,22 @@
+$LOAD_PATH.unshift("lib")
+require 'rubygems'
+require 'lib/ephemeral_response'
+
+# Don't create fixtures for the localhost domain
+EphemeralResponse::Configuration.white_list = 'localhost'
+
+EphemeralResponse::Configuration.expiration = 1
+EphemeralResponse.activate
+
+# Start an HTTP server on port 9876 using netcat
+IO.popen %(echo "HTTP/1.1 200 OK\n\n" | nc -l 9876)
+sleep 1
+
+# Make a request to the server started above
+# No new fixtures are created in spec/fixtures/ephemeral_response/
+uri = URI.parse('http://localhost:9876/')
+Net::HTTP.get(uri)
+
+# Fixtures are still created for Google
+uri = URI.parse('http://www.google.com/')
+Net::HTTP.get(uri)
